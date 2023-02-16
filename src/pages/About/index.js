656d76307de7d6aadf21edbff7b90164fc9054f7
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2023-02-17 01:08:26 +0530

    made some changes

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,25 +1,47 @@
 import React from 'react'
 import styles from './styles/about.modules.css'
+import './styles/about.modules.css'
+import backg from './assets/leaves.png'
+import { padding } from '@mui/system'
 
 const About = () => {
+  const data = {
+    "bgImage" : {
+      image: backg,
+      content: ['lorem']
+    }
+  }
   return (
     <div className={styles.main} style={
         {
-          background: "rgb(19,4,34)",
-          background: "linear-gradient(90deg, rgba(19,4,34,1) 0%, rgba(237,193,138,0.75) 50%, rgba(134,34,100,1) 100%)",
-          height: '130vh',
-          fontFamily: 'Lexend Exa, sans-serif'
+          height: '100vh',
+          fontFamily: 'Lexend Exa, sans-serif',
+          backgroundImage: `url(${backg})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          backgroundBlendMode: 'color-burn'
+          
+          // height: '100%'
         }
       }>
-        <div className={styles.flexMainContainer}>
-          <div className={styles.flexAbout} style={{height: '600px', width: '600px'}}>
+        <div className={styles.flexMainContainer} style={{
+         display: 'flex',
+         flexDirection: 'row-reverse',
+         justifyContent: 'center',
+         alignItems: 'center',
+        }}>
+          <aboutCircle className={styles.flexAbout}>
             <h2>About</h2>
-            <p style={{padding: '3rem'}}>lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit ametlorem ipsum dolor sit amet lorem ipsum dolor sit ametlorem ipsum dolor sit amet lorem ipsum dolor sit ametlorem ipsum dolor sit amet lorem ipsum dolor sit ametlorem ipsum dolor sit amet lorem ipsum dolor sit ametlorem ipsum dolor sit amet lorem ipsum dolor sit amet ipsum dolor sit ametlorem ipsum dolor sit amet lorem ipsum dolor sit ametlorem ipsum dolor sit amet</p>
-          </div>
-          <div className={styles.flexInnerContainer}>
-            <div className={styles.right}><h2>100k+</h2></div>
-            <div className={styles.left}><h2>150+</h2></div>
-            <div className={styles.right}><h2>500+</h2></div>
+            <p style={{padding: '3rem'}}>lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit ametlorem ipsum dolor sit amet lorem ipsum dolor sit ametlorem ipsum dolor sit amet lorem ipsum dolor sit ametlorem ipsum dolor sit amet lorem ipsum dolor sit ametlorem ipsum dolor sit amet lorem ipsum dolor sit amet</p>
+          </aboutCircle>
+          <div className={styles.flexInnerContainer} style={{
+            display: 'flex',
+            flexDirection: 'column',
+            margin: '2rem'
+          }}>
+            <circleRight className={styles.right}><h2>100k+</h2></circleRight>
+            <circleLeft className={styles.left}><h2>150+</h2></circleLeft>
+            <circleRight className={styles.right}><h2>500+</h2></circleRight>
           </div>
         </div>
       </div>
